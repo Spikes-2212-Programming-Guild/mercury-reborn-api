912de37781e1f48b03924a35f1c20e2d87586a26
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-01-10 18:27:48 +0200

    added routes to app

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const config = require('./config.json')
 
+const routes = require('./routes')
+
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/login', routes.login)
 
 app.listen(config.http.port, () => {
   console.log('started web server for mercury on port ' + config.http.port)
