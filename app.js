61c85daf7fe78ddf94ab184e92f0a2f8627f2298
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-01-10 17:58:12 +0200

    added basic app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const express = require('express')
+const config = require('./config.json')
+
+const app = express()
+
+app.listen(config.http.port, () => {
+  console.log('started web server for mercury on port ' + config.http.port)
+})
