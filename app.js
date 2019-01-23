4e7e0721f562449de93dc79b2a5ca9df7cbd314b
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-01-23 16:46:10 +0200

    changed eslint config

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import routes from './routes'
-import config from './config.json'
+import express from "express"
+import bodyParser from "body-parser"
+import routes from "./routes"
+import config from "./config.json"
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use('/login', routes.login)
+app.use("/login", routes.login)
 
 app.listen(config.http.port, () => {
-  console.log('started web server for mercury on port ' + config.http.port)
+  console.log("started web server for mercury on port " + config.http.port)
 })
