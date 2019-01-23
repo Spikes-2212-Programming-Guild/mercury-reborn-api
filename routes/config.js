63e5e6d037f74c678071a706717c2c7730e1d977
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-01-23 16:54:24 +0200

    added basic config route

---
+++ routes/config.js
@@ -0,0 +1,18 @@
+import { Router } from "express"
+import gameConfig from "../config.json"
+
+const scoutingForm = require(`./${gameConfig.data.form}`)
+
+const router = Router()
+
+router.get("/event-name", (req, res) => {
+  res.json(gameConfig.data.eventName)
+})
+
+router.get("/form", (req, res) => {
+  res.json(scoutingForm)
+})
+
+
+
+export {router as config}
