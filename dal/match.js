852a24d477a82d7987528c422b3df1bc6d4a615b
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-17 18:22:24 +0200

    added saveMatch function

--- dal/match.js
+++ dal/match.js
@@ -1,1 +1,13 @@
 import * as team from "./team"
+
+async function saveMatch(teamNumber, match) {
+  const query = {}
+  query[`${match.number}`] = match
+  return await team.updateTeam(teamNumber, {
+    $set: query
+  })
+}
+
+export {
+  saveMatch
+}
