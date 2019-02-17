eba0a74c415abb46edae2e8ff725ed027429fea9
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-17 20:12:29 +0200

    fixed spelling in TODO

--- bl/add-match.js
+++ bl/add-match.js
@@ -1,5 +1,5 @@
 //TODO add a function that is responsible for adding a match to the database.
-//TODO in case the team dosen't exist create an empty one in the database
+//TODO in case the team doesn't exist create an empty one in the database
 
 
 //TODO add a match.js route that calls the function on the path /match/submit
