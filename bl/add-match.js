61eb416146a1a2e5073fe1ae9d8c9491517f3545
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-17 20:29:38 +0200

    added addMatch function

--- bl/add-match.js
+++ bl/add-match.js
@@ -1,5 +1,18 @@
+import {saveMatch} from "../dal/match"
+import {insertTeam, teamExsits} from "../dal/team"
+
+async function addMatch(teamNumber, match) {
+  if(!teamExsits(teamNumber))
+    await insertTeam({number: teamNumber})
+  return await saveMatch(teamNumber, match)
+
+}
+
+export {
+  addMatch
+}
+
 //TODO add a function that is responsible for adding a match to the database.
 //TODO in case the team doesn't exist create an empty one in the database
-
 
 //TODO add a match.js route that calls the function on the path /match/submit
