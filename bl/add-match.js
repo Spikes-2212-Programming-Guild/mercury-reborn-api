ba2c240299c166d646eb98650651521b63058ed0
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-19 05:33:39 +0200

    added add-match logic to bl.

--- bl/add-match.js
+++ bl/add-match.js
@@ -12,7 +12,4 @@ export {
   addMatch
 }
 
-//TODO add a function that is responsible for adding a match to the database.
-//TODO in case the team doesn't exist create an empty one in the database
-
 //TODO add a match.js route that calls the function on the path /match/submit
