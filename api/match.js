87628ed72229269e07aac6c9fb05f77f10b6c168
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-23 14:07:19 +0200

    renamed form to match

--- api/match.js
+++ api/match.js
@@ -4,8 +4,8 @@ import {addMatch} from "../bl/add-match"
 const router = Router()
 
 router.post("/match/submit", (req, res) => {
-  const {teamNumber, form} = req.data
-  addMatch(teamNumber, form)
+  const {teamNumber, form: match} = req.data
+  addMatch(teamNumber, match)
     .then(() => res.end())
 })
 
