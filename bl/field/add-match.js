ee46591fd8cee1752751f41a176ec0fc722f4ecc
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-02 18:40:55 +0200

    fixed field bl

--- bl/field/add-match.js
+++ bl/field/add-match.js
@@ -2,6 +2,7 @@ import {saveMatch} from "../../dal/field/match"
 import {insertTeam, teamExsits} from "../../dal/field/team"
 
 async function addMatch(teamNumber, match) {
+  delete match.team
   if(! (await teamExsits(teamNumber))) {
     await insertTeam({number: teamNumber, matches: {}})
   }
