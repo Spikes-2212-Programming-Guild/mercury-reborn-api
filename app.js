d842550f20c3599a1c5684fecc80818d361ab137
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-02 16:54:28 +0200

    fixed initialization

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ import express from "express"
 import bodyParser from "body-parser"
 import api from "./api"
 import config from "./config.json"
-import {exp} from "./dal"
+import {initializeConnection} from "./dal"
 
-const {initializeConnection} = exp
 
 const app = express()
 
@@ -12,9 +11,10 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use("/login", api.login)
 app.use("/config", api.config)
-app.use("/match", api.match)
+app.use("/scouting/field", api.scouting.field)
+app.use("/scouting/pit", api.scouting.pit)
 
-initializeConnection(config.mongo.url, config.mongo.db, config.mongo.collection)
+initializeConnection(config.mongo.url, config.mongo.db, config.mongo.collectionNames)
   .then(() => {
     app.listen(config.http.port, () => {
       console.log("started web server for mercury on port " + config.http.port)
