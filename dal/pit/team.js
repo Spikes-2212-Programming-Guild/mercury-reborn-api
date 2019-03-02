e0afb7325a66b7b67ca3e4fcefb564a69f909bb1
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-02 18:35:59 +0200

    completed pit dal

--- dal/pit/team.js
+++ dal/pit/team.js
@@ -2,11 +2,19 @@ let teams = null
 
 
 async function addPitScouting(team, form) {
-  return teams.insert({team, form})
+  return await teams.insertOne({team, form})
+}
+
+async function pitScoutingExists(team) {
+  return !!(await teams.findOne({team}))
+}
+
+async function dropPitScouting(team) {
+  return await teams.dropOne({team})
 }
 
 async function getPitScouting(team) {
-  return (await teams.find({team}).project({id:0, team:0})).form
+  return (await teams.findOne({team}).project({id:0, team:0})).form
 }
 
 
@@ -17,5 +25,7 @@ function setCollection(collection) {
 export {
   addPitScouting,
   getPitScouting,
-  setCollection
+  setCollection,
+  dropPitScouting,
+  pitScoutingExists
 }
