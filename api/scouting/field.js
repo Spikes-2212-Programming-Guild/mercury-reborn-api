b16e9fbe4fa62623852b29aa4139b0aaa6e05b3c
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-02 16:16:33 +0200

    separated scouting api

--- api/scouting/field.js
+++ api/scouting/field.js
@@ -1,12 +1,12 @@
 import {Router} from "express"
-import {addMatch} from "../bl/add-match"
+import {addMatch} from "../../bl/field/add-match"
 
 const router = Router()
 
-router.post("/submit", (req, res) => {
+router.post("/match/submit", (req, res) => {
   const {teamNumber, match} = req.body
   addMatch(teamNumber, match)
     .then(() => res.end())
 })
 
-export {router as match}
+export {router as field}
