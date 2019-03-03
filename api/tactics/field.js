693f79369fa170867aace3950973d7d7a3ddfd57
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-03 16:34:18 +0200

    added getter to tactics/field api

--- api/tactics/field.js
+++ api/tactics/field.js
@@ -1,10 +1,19 @@
 import {Router} from "express"
-import { getAllSavedMatchNames } from "../../bl/field"
+import { getAllSavedMatchNames, getFieldScouting } from "../../bl/field"
 
 const router = Router()
 
 router.get("/match-names", (req, res) => {
   getAllSavedMatchNames().then(matchNames => res.json(matchNames))
 })
+
+router.get("/match/:team_id/:match_name", (req, res) => {
+  const {team_id, match_name} = req.params
+
+  getFieldScouting(team_id, match_name)
+    .then(match => res.json(match))
+
+})
+
 
 export {router as field}
