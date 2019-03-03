e890e790369560d3b98a105107b75b48ec315f8b
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-03 16:32:22 +0200

    added get to pit api

--- api/scouting/pit.js
+++ api/scouting/pit.js
@@ -1,10 +1,18 @@
 import {Router} from "express"
-import {savePitScouting} from "../../bl/pit"
+import { getPitScouting, savePitScouting } from "../../bl/pit"
 
 const router = Router()
 
 router.post("/team/submit", (req, res) => {
   savePitScouting(req.body.form).then(() => res.end())
+})
+
+router.get("/team/:team_id", (req, res) => {
+  const {team_id} = req.params
+
+  getPitScouting(team_id)
+    .then(pitScouting => res.json(pitScouting))
+
 })
 
 export {router as pit}
